d0c13248429c0ad7b5ddc503029913eeeb471291
Ana Carolina Aquino <113844035+AnaCarolinaAquino@users.noreply.github.com>
2022-10-15 20:44:10 -0300

    innitial commit
    Projeto individual 3 do curso Programadores Cariocas em parceria com Resilia. Página web com Codificador e Decodificador de Criptografia de César e Base64.

---
+++ projeto-individual-3/script.js
@@ -0,0 +1,85 @@
+let codificar = document.querySelector('#codificar');
+let decodificar = document.querySelector('#decodificar');
+let selecione = document.querySelector('.select');
+let incremento = document.querySelector('.chave-container');
+let btn = document.querySelector('.button');
+let radiobtn = document.querySelector('.radio-button');
+
+// Incremento da Cifra de César
+selecione.addEventListener('click', function () {
+  if (selecione.value == 'cifra') {
+    incremento.style.display = 'block';
+  } else {
+    incremento.style.display = 'none';
+  }
+});
+
+// Função da Cifra de César
+
+function cifraCesar() {
+  let msg = document.querySelector('#mensagem').value;
+  let chave = parseInt(document.querySelector('#rangenumber').value);
+  let saida = '';
+
+  if (codificar.checked) {
+    for (let i = 0; i < msg.length; i++) {
+      if (msg[i] === msg[i].toUpperCase()) {
+        saida += String.fromCharCode(
+          ((msg.charCodeAt(i) + chave - 65) % 26) + 65
+        );
+      } else {
+        saida += String.fromCharCode(
+          ((msg.charCodeAt(i) + chave - 97) % 26) + 97
+        );
+      }
+    }
+    return saida;
+  } else if (decodificar.checked) {
+    for (let i = 0; i < msg.length; i++) {
+      if (msg.charCodeAt(i) >= 97 && msg.charCodeAt(i) <= 122) {
+        saida += String.fromCharCode(
+          ((msg.charCodeAt(i) - 97 - chave + 26) % 26) + 97
+        );
+      } else if (msg.charCodeAt(i) >= 65 && msg.charCodeAt(i) <= 90) {
+        saida += String.fromCharCode(
+          ((msg.charCodeAt(i) - 65 - chave + 26) % 26) + 65
+        );
+      } else {
+        saida += String.fromCharCode(msg.charCodeAt(i));
+      }
+    }
+    return saida;
+  }
+}
+
+// Função de Base64
+
+function base64() {
+  let mensagem = document.querySelector('#mensagem').value;
+
+  if (codificar.checked) {
+    let codificado = btoa(mensagem);
+    return codificado;
+  } else if (decodificar.checked) {
+    let decodificado = atob(mensagem);
+    return decodificado;
+  }
+}
+
+// Botões
+radiobtn.addEventListener('click', function () {
+  if (codificar.checked) {
+    btn.innerHTML = 'Codificar Mensagem!';
+  } else if (decodificar.checked) {
+    btn.innerHTML = 'Decodificar Mensagem!';
+  }
+});
+
+btn.addEventListener('click', function (event) {
+  event.preventDefault();
+  if (selecione.value === 'base64') {
+    resultado.innerText = base64();
+  } else if (selecione.value === 'cifra') {
+    resultado.innerText = cifraCesar();
+  }
+});
